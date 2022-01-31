e77b5752b8eab9333c997e960750bd9de713c432
AlejandroSO-web <letur_alex@hotmail.com>
2022-01-31 12:00:20 +0100

    bakeryCART

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -13,57 +13,8 @@ class CartController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('cart/index.html.twig', [
+        return $this->render('partials/cart.part.html.twig', [
             'controller_name' => 'CartController',
         ]);
     }
-}
-
-
-<?php
-
-namespace ProyectoWeb\app\controllers;
-
-use Psr\Container\ContainerInterface;
-
-use ProyectoWeb\entity\Product;
-
-use ProyectoWeb\exceptions\QueryException;
-
-use ProyectoWeb\exceptions\NotFoundException;
-
-use ProyectoWeb\database\Connection;
-
-use ProyectoWeb\repository\ProductRepository;
-
-use ProyectoWeb\core\App;
-
-class CartController
-
-{
-
-    protected $container;
-
-   
-
-    // constructor receives container instance
-
-    public function __construct(ContainerInterface $container) {
-
-        $this->container = $container;
-
-    }
-
-    public function render($request, $response, $args) {
-
-        extract($args);
-
-        $title = " Carrito ";
-
-        $withCategories = false;
-
-        return $this->container->renderer->render($response, "cart.view.php", compact('title', 'withCategories'));
-
-    }
-
 }
