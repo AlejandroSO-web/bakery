6eb9eb310a6d83183846603dedc100adf7eeabe0
AlejandroSO-web <letur_alex@hotmail.com>
2022-02-14 17:56:44 +0100

    bakeryF

--- public/js/movimiento.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $("#imagen1").mouseover(function(){
-        $("#imagen1").fadeIn('slow','linear');
-
-    })
-});
---
+++ public/js/opacity.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#imagen1").click(function(){
+        $("#imagen1").css('opacity', '0.5');
+
+    });
+});
